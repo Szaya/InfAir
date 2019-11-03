f3099121459c9ff8257bf9d1ed93acfbc6f39e07
Szaya <resb014@gmail.com>
2019-11-03 17:21:52 +0100

    Make Base app, which isn't work at this point.

---
+++ src/main/java/hu/elte/InfAir/InfAirApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.InfAir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfAirApplication.class, args);
+	}
+
+}
