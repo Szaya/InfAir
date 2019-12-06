f9ca773e01def211c2c693bbaf151a4d70fad6e5
f9ld49 <kriszta9607@gmail.com>
2019-12-06 14:07:31 +0100

    sec pass

--- src/main/java/hu/elte/InfAir/Security/WebSecurityConfig.java
+++ src/main/java/hu/elte/InfAir/Security/WebSecurityConfig.java
@@ -30,7 +30,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/h2/**", "/users/register").permitAll()   // important!
                 .anyRequest().authenticated()
                 .and()
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO user (name, role,username,password) VALUES ('Józsi', 'ROLE_USER','asd1','$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..');
 INSERT INTO user (name, role,username,password) VALUES ('Pista', 'ROLE_ADMINISTRATOR','asd2','$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..');
 INSERT INTO user (name, role,username,password) VALUES ('Péter', 'ROLE_USER','asd3','$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..');
-INSERT INTO user (name, role,username,password) VALUES ('Sanyi', 'ROLE_ADMINISTRATOR','username','dXNlcm5hbWU6cGFzc3dvcmQ=');
+INSERT INTO user (name, role,username,password) VALUES ('Sanyi', 'ROLE_ADMINISTRATOR','asd','YXNkOmFzZA==');
 
 INSERT INTO flight (startpoint, endpoint, launch, arrival, cost, sale) VALUES ('Budapest', 'London', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 65555, 0.8);
 INSERT INTO flight (startpoint, endpoint, launch, arrival, cost, sale) VALUES ('London', 'Budapest', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 65555, 1.0);
